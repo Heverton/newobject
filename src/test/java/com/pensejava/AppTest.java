7fe8caaa2a350270be763fae2e4fef49f11d25fc
Heverton da Silva Cruz <heverton@users.noreply.github.com>
2016-12-12 13:19:44 -0200

    Add version github.

--- src/test/java/com/pensejava/AppTest.java
+++ src/test/java/com/pensejava/AppTest.java
@@ -19,15 +19,15 @@ public class AppTest {
         List<Pessoa> pessoas = (List<Pessoa>) new PersistenceProviderNew().newProvider(15,
                 new Pessoa(15,"Heverton", "Silva", new Endereco(15, "QMS 03"), "986325705", "Rosanilza", "Antonio"));
 
-        for (Pessoa p : pessoas){
-            System.out.print(p.getId()+" - ");
-            System.out.print(p.getNome()+" - ");
-            System.out.print(p.getNomeMae()+" - ");
-            System.out.print(p.getEnderecos().getId()+" - ");
-            System.out.print(p.getEnderecos().getNome()+" - ");
-            System.out.print(p.getSobrenome()+" - ");
-            System.out.print(p.getTelefone());
-        }
+//        for (Pessoa p : pessoas){
+//            System.out.print(p.getId()+" - ");
+//            System.out.print(p.getNome()+" - ");
+//            System.out.print(p.getNomeMae()+" - ");
+//            System.out.print(p.getEnderecos().getId()+" - ");
+//            System.out.print(p.getEnderecos().getNome()+" - ");
+//            System.out.print(p.getSobrenome()+" - ");
+//            System.out.print(p.getTelefone());
+//        }
 
         Assert.assertEquals("É uma lista de Pessoa? ", 15, pessoas.size());
     }
@@ -36,19 +36,19 @@ public class AppTest {
     public void onetomany1(){
         List<PessoaList> pessoas = (List<PessoaList>) new PersistenceProviderNew().newProvider(1, new PessoaList());
 
-        for (PessoaList p : pessoas){
-            System.out.println("getId = "+p.getId());
-            System.out.println("getNome = "+p.getNome());
-            System.out.println("getNomeMae = "+p.getNomeMae());
-
-            for (Endereco end : p.getEnderecos()) {
-                System.out.println("end.getId = "+end.getId() );
-                System.out.println("end.getNome = "+end.getNome() );
-            }
-
-            System.out.println("getSobrenome = "+p.getSobrenome());
-            System.out.println("getTelefone = "+p.getTelefone());
-        }
+//        for (PessoaList p : pessoas){
+//            System.out.println("getId = "+p.getId());
+//            System.out.println("getNome = "+p.getNome());
+//            System.out.println("getNomeMae = "+p.getNomeMae());
+//
+//            for (Endereco end : p.getEnderecos()) {
+//                System.out.println("end.getId = "+end.getId() );
+//                System.out.println("end.getNome = "+end.getNome() );
+//            }
+//
+//            System.out.println("getSobrenome = "+p.getSobrenome());
+//            System.out.println("getTelefone = "+p.getTelefone());
+//        }
 
         Assert.assertEquals("É uma lista de Pessoa? ", 1, pessoas.size());
     }
@@ -57,15 +57,15 @@ public class AppTest {
     public void onetomany2(){
         List<Pessoa> pessoas = (List<Pessoa>) new PersistenceProviderNew().newProvider(15,  new Pessoa());
 
-        for (Pessoa p : pessoas){
-            System.out.print(p.getId()+" - ");
-            System.out.print(p.getNome()+" - ");
-            System.out.print(p.getNomeMae()+" - ");
-            System.out.print(p.getEnderecos().getId()+" - ");
-            System.out.print(p.getEnderecos().getNome()+" - ");
-            System.out.print(p.getSobrenome()+" - ");
-            System.out.print(p.getTelefone()+"\n");
-        }
+//        for (Pessoa p : pessoas){
+//            System.out.print(p.getId()+" - ");
+//            System.out.print(p.getNome()+" - ");
+//            System.out.print(p.getNomeMae()+" - ");
+//            System.out.print(p.getEnderecos().getId()+" - ");
+//            System.out.print(p.getEnderecos().getNome()+" - ");
+//            System.out.print(p.getSobrenome()+" - ");
+//            System.out.print(p.getTelefone()+"\n");
+//        }
 
         Assert.assertEquals("É uma lista de Pessoa? ", 15, pessoas.size());
     }
